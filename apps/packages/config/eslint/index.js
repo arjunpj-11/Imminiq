ca147a9b1f5d8816ff80560c4a5a088bdef2cdcb
arjunpj-11 <arjunpj11@gmail.com>
2026-05-06 19:18:32 +0530

    Initial monorepo folder structure

---
+++ apps/packages/config/eslint/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    es2022: true,
+    node: true,
+    browser: true
+  },
+  rules: {
+    "no-unused-vars": "warn",
+    "no-console": "off"
+  }
+};
